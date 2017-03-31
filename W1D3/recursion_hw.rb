fc455eb6df7bdccaa94597d24f9c716a9e8babbf
Yong Lin <lynilir@Yongs-MacBook-Pro.local>
2017-03-31 01:04:59 -0700

    Add w1d3 recursion exercises

---
+++ W1D3/recursion_hw.rb
@@ -0,0 +1,78 @@
+# Read the problem statement to make sure you fully understand the problem
+# Identify the base case(s)
+# Determine the inductive step
+# Write the function
+# Run the provided test cases to verify you have a working solution
+
+
+# Write a function sum_to(n) that uses recursion to calculate the sum from 1 to n (inclusive of n).
+def sum_to(n)
+  return if n < 0
+  return n if n <= 1
+  sum_to(n - 1) + n
+end
+  # Test Cases
+p  sum_to(5)  # => returns 15
+p  sum_to(1)  # => returns 1
+p  sum_to(9)  # => returns 45
+p  sum_to(-8)  # => returns nil
+
+
+# Write a function add_numbers(nums_array) that takes in an array of Fixnums and returns the sum of those numbers. Write this method recursively.
+def add_numbers(nums_array)
+  return nil if nums_array == []
+  return nums_array.pop if nums_array.length == 1
+  nums_array.pop + add_numbers(nums_array)
+end
+  # Test Cases
+p  add_numbers([1,2,3,4]) # => returns 10
+p  add_numbers([3]) # => returns 3
+p  add_numbers([-80,34,7]) # => returns -39
+p  add_numbers([]) # => returns nil
+
+
+# Let's write a method that will solve Gamma Function recursively. The Gamma Function is defined Γ(n) = (n-1)!.
+def factorial(n)
+  return nil if n == 0
+  return 1 if n <= 1
+  n * factorial(n-1)
+end
+
+def gamma_fnc(n)
+  factorial(n-1)
+end
+
+  # Test Cases
+p  gamma_fnc(0)  # => returns nil
+p  gamma_fnc(1)  # => returns 1
+p  gamma_fnc(4)  # => returns 6
+p  gamma_fnc(8)  # => returns 5040
+
+# Write a function ice_cream_shop(flavors, favorite) that takes in an array of ice cream
+# flavors available at the ice cream shop, as well as the user's favorite ice cream flavor.
+# Recursively find out whether or not the shop offers their favorite flavor.
+def ice_cream_shop(flavors, favorite)
+  return flavors[0] == favorite if flavors.length == 1
+  return true if flavors[0] == favorite
+  ice_cream_shop(flavors[1..-1], favorite)
+end
+  # Test Cases
+p  ice_cream_shop(['vanilla', 'strawberry'], 'blue moon')  # => returns false
+p  ice_cream_shop(['pistachio', 'green tea', 'chocolate', 'mint chip'], 'green tea')  # => returns true
+p  ice_cream_shop(['cookies n cream', 'blue moon', 'superman', 'honey lavender', 'sea salt caramel'], 'pistachio')  # => returns false
+p  ice_cream_shop(['moose tracks'], 'moose tracks')  # => returns true
+
+
+# Write a function reverse(string) that takes in a string and returns it reversed.
+
+def reverse(string)
+  return string if string.length <= 1
+  string[-1] + reverse(string[0..-2])
+end
+  # Test Cases
+p reverse("house") # => "esuoh"
+p reverse("dog") # => "god"
+p reverse("atom") # => "mota"
+p reverse("q") # => "q"
+p reverse("id") # => "di"
+p reverse("") # => ""
